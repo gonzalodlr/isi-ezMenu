dd82ed198465eed318e7f1289e621b746c0802db
gonzalodlr <gonzalo.reyes@alu.uclm.es>
2024-03-31 20:03:40 +0200

    Mejorando interfaz y backend, pequeño bug al buscar por primera vez una comida

--- backend/routes/food_route.py
+++ backend/routes/food_route.py
@@ -12,87 +12,100 @@ load_dotenv()
 
 # Use the variable names as defined in .env file
 api_key = os.getenv("API_KEY")
+# Ruta de la carpeta 'assets'
+route = '../assets/'
 
-def get_soup():
-    filename = "../assets/results.json"
-    with open(filename, 'r') as file:
-        response = json.load(file)
-    results = response.get("results", [])
-    foods = []
-    for result in results:
-        name = result.get("name")
-        id = result.get("id")
-        description = result.get("description")
-        thumbnail_url = result.get("thumbnail_url")
-        prep_time_minutes = result.get("prep_time_minutes")
-        cook_time_minutes = result.get("cook_time_minutes")
-        num_servings = result.get("num_servings")
-        instructions = result.get("instructions")
-        sections = result.get("sections")
-        user_ratings = result.get("user_ratings")
-        video_url = result.get("video_url")
-        price = result.get("price")
-        ingredients = result.get("ingredients")
-        nutrition = result.get("nutrition")
+def exits_json(filename):
+    # Verificar si la carpeta 'assets' existe
+    if os.path.exists(route) and os.path.isdir(route):
+        # Lista de archivos en la carpeta 'assets'
+        archivos = os.listdir(route)
+        # Verificar si hay algún archivo 'search.json'
+        if filename + '.json' in archivos:
+            return True
+        else:
+            return False
+    else:
+        # La carpeta 'assets' no existe
+        return False
 
-        # Crear un objeto de la clase Food
-        food = Food(name, id, description, thumbnail_url, prep_time_minutes, cook_time_minutes, num_servings, instructions, sections, user_ratings, video_url, price, ingredients, nutrition)
-        # Agregar el objeto a la lista de objetos
-        foods.append(food)
-    return foods
+def search_json_data(filename):
+    # Ruta de la carpeta 'assets'
+    route = '../assets/'
+
+    # Verificar si la carpeta 'assets' existe
+    if os.path.exists(route) and os.path.isdir(route):
+        # Lista de archivos en la carpeta 'assets'
+        archivos = os.listdir(route)
+
+        # Verificar si hay algún archivo 'search.json'
+        if filename + '.json' in archivos:
+            with open(route + filename + '.json', 'r') as file:
+                response = json.load(file)
+            results = response.get("results", [])
+            foods = []
+            for result in results:
+                name = result.get("name")
+                id = result.get("id")
+                description = result.get("description")
+                thumbnail_url = result.get("thumbnail_url")
+                prep_time_minutes = result.get("prep_time_minutes")
+                cook_time_minutes = result.get("cook_time_minutes")
+                num_servings = result.get("num_servings")
+                instructions = result.get("instructions")
+                sections = result.get("sections")
+                user_ratings = result.get("user_ratings")
+                video_url = result.get("video_url")
+                price = result.get("price")
+                ingredients = result.get("ingredients")
+                nutrition = result.get("nutrition")
+
+                # Crear un objeto de la clase Food
+                food = Food(name, id, description, thumbnail_url, prep_time_minutes, cook_time_minutes, num_servings, instructions, sections, user_ratings, video_url, price, ingredients, nutrition)
+                # Agregar el objeto a la lista de objetos
+                foods.append(food)
+            return foods
+        else:
+            return False
+    else:
+        # La carpeta 'assets' no existe
+        return False
 
 @app.route('/search-foods', methods=['GET'])
 def search_foods():
     searchTerm = request.args.get('searchTerm')
-    if searchTerm == "soup":
-        foods = get_soup()
+    if exits_json(searchTerm):
+        foods = search_json_data(searchTerm)
         serialized_foods = serialize_food(foods)
         return jsonify(serialized_foods)
-    
-    url = "https://tasty.p.rapidapi.com/recipes/list"
-    querystring = {
-        "from": "0",
-        "size": "9",
-        "q": searchTerm  # Use the provided search term
-    }
+    else:
+        url = "https://tasty.p.rapidapi.com/recipes/list"
+        querystring = {
+            "from": "0",
+            "size": "9",
+            "q": searchTerm  # Use the provided search term
+        }
 
-    headers = {
-        "X-RapidAPI-Key": api_key,
-        "X-RapidAPI-Host": "tasty.p.rapidapi.com"
-    }
+        headers = {
+            "X-RapidAPI-Key": api_key,
+            "X-RapidAPI-Host": "tasty.p.rapidapi.com"
+        }
 
-    try:
-        response = requests.get(url, headers=headers, params=querystring)
-        response.raise_for_status()  # Handle HTTP errors
-        data = response.json()        
-        food_array = []
-        # Obtain each result in an array
-        for result in data["results"]:
-            name = result.get("name")
-            id = result.get("id")
-            description = result.get("description")
-            thumbnail_url = result.get("thumbnail_url")
-            prep_time_minutes = result.get("prep_time_minutes")
-            cook_time_minutes = result.get("cook_time_minutes")
-            num_servings = result.get("num_servings")
-            instructions = result.get("instructions")
-            sections = result.get("sections")
-            user_ratings = result.get("user_ratings")
-            video_url = result.get("video_url")
-            price = result.get("price")
-            ingredients = result.get("ingredients")
-            nutrition = result.get("nutrition")
-            
-            food = Food(name, id, description, thumbnail_url, prep_time_minutes, cook_time_minutes, num_servings, instructions, sections, user_ratings, video_url, price, ingredients, nutrition)
-            print(food)
-            food_array.append(food)
+        try:
+            response = requests.get(url, headers=headers, params=querystring)
+            if response.status_code != 200:
+                return jsonify({"error": "The request was not successful"}), 500
+            # Guarda la respuesta JSON en un archivo
+            with open('../assets/' + searchTerm + '.json', 'w') as file:
+                json.dump(response.json(), file)
+            # Leer el archivo JSON en un array de objetos de la clase Food
+            foods = search_json_data(searchTerm)
+            serialized_foods = serialize_food(foods)
+            return jsonify(serialized_foods)
 
-        serialized_foods = serialize_food(foods)
-        return jsonify(serialized_foods)
-
-    except requests.exceptions.RequestException as e:
-        # Handle any request error
-        return jsonify({"error": str(e)}), 500
+        except requests.exceptions.RequestException as e:
+            # Handle any request error
+            return jsonify({"error": str(e)}), 500
 
 def serialize_food(food_list):
     serialized_food = []
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -128,41 +128,8 @@ const showHTML = () => {
 // Obtener todas las tarjetas
 const cards = document.querySelectorAll(".card");
 const imageUrl = "./assets/img/burger.jpg"; // URL de la imagen de fondo
-const number = 0; // Número de la tarjeta
-
-// Iterar sobre cada tarjeta y modificar sus atributos
-cards.forEach((card) => {
-  card.style.backgroundImage = `url(${imageUrl})`;
-  card.style.backgroundSize = "cover";
-  card.style.backgroundPosition = "center";
-  card.style.backgroundAttachment = "fixed";
-  card.style.backgroundClip = "border-box";
-  card.style.backgroundRepeat = "no-repeat";
-  card.style.height = "300px";
-  card.style.width = "300px";
-  card.style.borderRadius = "10px";
-  card.style.color = "white";
-  card.style.fontWeight = "bold";
-  card.style.textAlign = "center";
-  card.style.padding = "20px";
-  card.style.margin = "20px";
-  card.style.display = "flex";
-  card.style.justifyContent = "center";
-  card.style.alignItems = "center";
-  card.style.flexDirection = "column";
-  card.style.boxShadow = "0 4px 8px rgba(0,0,0,0.1)";
-  card.style.transition = "transform 0.3s";
-  card.style.cursor = "pointer";
-  card.style.transform = "scale(1)";
-  card.style.overflow = "hidden";
-    card.style.position = "relative";
-});
 
 // Funcion que se ejecuta cuando el boton Search es presionado
-/*document.getElementById("searchButton").addEventListener("click", function() {
-  var searchTerm = document.getElementById("searchInput").value;
-  buscarComidas(searchTerm);
-});*/
 document.getElementById("searchForm").addEventListener("submit", function(event) {
   event.preventDefault(); // Evitar el comportamiento predeterminado del formulario
   var searchTerm = document.getElementById("searchInput").value;
@@ -177,8 +144,9 @@ function buscarComidas(searchTerm) {
   fetch('http://127.0.0.1:5000/search-foods?searchTerm=' + searchTerm) // Suponiendo que el backend escucha en '/buscar-comidas'
   .then(response => response.json())
   .then(data => {
-
+    console.log(data); // Compruebo respuesta del backend
     const foodList = JSON.parse(data);
+
     foodList.forEach(food => {
       /*const ima = food.Thumbnail_URL;
           const name = food.Price;
@@ -218,8 +186,6 @@ function buscarComidas(searchTerm) {
     
     });*/
     
-      // Manejar los datos recibidos (en este caso, se supone que recibimos un array de objetos tipo Food)
-      console.log(data); // Puedes hacer lo que necesites con los datos
   })
   .catch(error => {
       console.error('Error al buscar comidas:', error);
