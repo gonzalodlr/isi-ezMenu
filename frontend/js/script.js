7b8a507aadf98c0153ae4762a6df99b152233fc6
werth2002 <114737693+werth2002@users.noreply.github.com>
2024-03-31 15:44:54 +0200

    Update script.js
    Codigo para crear la seleccion de productos

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -50,6 +50,47 @@ function buscarComidas(searchTerm) {
   fetch('http://127.0.0.1:5000/search-foods?searchTerm=' + searchTerm) // Suponiendo que el backend escucha en '/buscar-comidas'
   .then(response => response.json())
   .then(data => {
+
+    const foodList = JSON.parse(data);
+    foodList.forEach(food => {
+      /*const ima = food.Thumbnail_URL;
+          const name = food.Price;
+          const n1 = name.consumption_portion;
+          console.log(`Nombre del alimento: ${ima}`);*/
+      const p = food.Price;
+      const p1= p.portion;
+        const article = document.createRange().createContextualFragment(`
+          <article>
+            <div class="item">
+              <figure>
+                <img
+                  src="${food.Thumbnail_URL}"
+                  alt="producto"
+                />
+              </figure>
+              <div class="info-product">
+                <h2>${food.Name}</h2>
+                <p class="price">$${p1}</p>
+                <button class="btn-add-cart">Añadir al carrito</button>
+              </div>
+            </div>
+          </article>
+        `);
+  
+        const container = document.querySelector(".container-items"); // Corregí el selector
+      container.append(article);
+  
+      
+      })
+    //window.location.href = "carrito.html?foodList=" + encodeURIComponent(JSON.stringify(foodList));
+    // Itera sobre la lista de alimentos y obtén el valor del campo "Name"
+    /*foodList.forEach(food => {
+        const name = food.Price;
+        const n1 = name.consumption_portion;
+        console.log(`Nombre del alimento: ${n1}`);
+    
+    });*/
+    
       // Manejar los datos recibidos (en este caso, se supone que recibimos un array de objetos tipo Food)
       console.log(data); // Puedes hacer lo que necesites con los datos
   })
